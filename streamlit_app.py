5be56086bec806cc188d8c285b2dc22ee8e14084
divya bhatia <68097340+divyabhatia519@users.noreply.github.com>
2023-10-14 19:59:22 +0530

    Update streamlit_app.py
    Added indexes like 'Avocado' & 'Strawberries'

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,20 +5,17 @@ streamlit.title('my parents new healthy dinner')
 streamlit.header('breakfast menu')
 
 streamlit.text('🍌🥭 omega 3 & blueberry oatmeal 🥝🍇')
-
 streamlit.text('🍌🥭 kale, spinach and roxket smoothie 🥝🍇')
-
 streamlit.text('🍌🥭 hard boiled egg 🥝🍇')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas 
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
