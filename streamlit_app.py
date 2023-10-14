cb87562645aecfd73c1cbb451db7cbffbcd7be30
divya bhatia <68097340+divyabhatia519@users.noreply.github.com>
2023-10-14 18:20:03 +0530

    Update streamlit_app.py
    imported pandas library

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,9 @@ streamlit.text('🍌🥭 kale, spinach and roxket smoothie 🥝🍇')
 streamlit.text('🍌🥭 hard boiled egg 🥝🍇')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas 
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
