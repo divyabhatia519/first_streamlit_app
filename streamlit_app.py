385f5d17f55d6fda8cf2fae46297fb65841d2ad3
divya bhatia <68097340+divyabhatia519@users.noreply.github.com>
2023-10-14 18:14:32 +0530

    Update streamlit_app.py
    added the key points of the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,9 @@ import streamlit
 streamlit.title('my parents new healthy dinner')
 
 streamlit.title('breakfast menu')
+
+streamlit.title('omega 3 & blueberry oatmeal')
+
+streamlit.title('kale, spinach and roxket smoothie')
+
+streamlit.title('hard boiled egg')
