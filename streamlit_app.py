b97c44b95bc829117943ee69e1294e621b8fbfe4
divya bhatia <68097340+divyabhatia519@users.noreply.github.com>
2023-10-17 13:40:07 +0530

    Update streamlit_app.py
    What fruit would you like to add?

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,10 @@ streamlit.dataframe(my_data_rows)
 
 
 
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+streamlit.write('Thanks for addidng ', add_my_fruit)
+
+
 
 
 
